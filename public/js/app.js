cf3417a16b37254d472ac45b2853fa1d14e21ff6
Siva Surya <babu.sivasurya@gmail.com>
2020-07-31 21:58:13 +0530

    setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -10,7 +10,7 @@ weatherForm.addEventListener('submit', (e) => {
 
     e.preventDefault()
 
-    const url = 'http://localhost:3000/weather?address=' + search.value
+    const url = '/weather?address=' + search.value
 
     fetch(url).then((response) => {
 
